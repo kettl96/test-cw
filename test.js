5a463a22c2ccb9430a8e1a280928b6a9b40e39cc
kettl96 <kettl110e4@gmail.com>
2022-06-12 05:21:22 +0300

    cw

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-let foo = (a) => (b) => a*b 
+// let foo = (a) => (b) => a*b 
+// foo(5)(5)
 
-
-
-
-foo(5)(5)
+function solution(str) {
+  let res = []
+  str.split('').forEach((e, i) => { e.toUpperCase() == e ? `${res.splice(i + 2, 0, ' '), res.push(e)}` : res.push(e) })
+  return res.join('')
+}
+console.log(solution('camelCase'));
+console.log(solution('camelCasingTest'));
